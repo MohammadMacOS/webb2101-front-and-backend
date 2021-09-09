8183196fd9e8ad9ebfac4a2ee7579eedbca17cfb
MohammadMacOS <mh.haydar92@gmail.com>
2021-09-09 14:38:27 +0200

    lade till POST request för att skapa användare

--- backend/frontend/src/App.js
+++ backend/frontend/src/App.js
@@ -1,18 +1,82 @@
 import './App.css';
-import {useState} from "react";
+import { useState } from "react";
+import http from './utils/api/UsersApi'
 
 function App() {
     const [text, setText] = useState('paragraph')
-  return (
-    <div>
-      <h1>Users API with Axios</h1>
-        <h3>'npm install axios'</h3>
-        <p>{ text }</p>
-        <button onClick={() => {
-            setText('New text')}}>New text
-        </button>
-    </div>
-  );
+
+    function alive() {
+        http.get('/')
+            .then(function (response) {
+                console.log(response.data)
+                setText(response.data)
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error)
+                return 'Error'
+            })
+            .then(function () {
+                // always executed
+            })
+    }
+
+    function getUsers(){
+        http.get('/users')
+            .then(function (response) {
+                console.log(response.data)
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
+    }
+
+    function getUserById(id){
+        http.get(`/users/${ id }`)
+            .then(function (response) {
+                console.log(response.data)
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
+    }
+
+
+    function createUser(name, age, gender) {
+        const payload = {
+            "name": name,
+            "age": age,
+            "gender": gender
+        }
+        http.post('/users', payload)
+            .then(function (response) {
+                console.log(response.data)
+            })
+            .catch(function (error){
+                console.log(error)
+            })
+
+    }
+
+
+
+    return (
+        <div>
+            <h1>Users API with Axios</h1>
+            <h3>'npm install axios'</h3>
+            <p>{ text }</p>
+
+            <button onClick={ () => {
+                setText('New Text')
+            } }>New text
+            </button>
+            <button onClick={ alive }>alive</button>
+            <button onClick={ getUsers }>getUsers</button>
+            <button onClick={ function() { getUserById(10)} }>getUserById</button>
+            <button onClick={ function () {createUser('Ada', 18, 'female')
+            } }>CreateUser</button>
+        </div>
+    );
 }
 
 export default App;
