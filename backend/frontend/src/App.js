28bde43b9249796a941f6445669a4f45d1bca3e9
MohammadMacOS <mh.haydar92@gmail.com>
2021-09-09 12:05:50 +0200

    Rensat filer samt skapat en grund med useState och en button

---
+++ backend/frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import {useState} from "react";
+
+function App() {
+    const [text, setText] = useState('paragraph')
+  return (
+    <div>
+      <h1>Users API with Axios</h1>
+        <h3>'npm install axios'</h3>
+        <p>{ text }</p>
+        <button onClick={() => {
+            setText('New text')}}>New text
+        </button>
+    </div>
+  );
+}
+
+export default App;
